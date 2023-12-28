8cc4c5b2b2ac02c2c7f8a43be5e0bf9e7839ccbd
Mario Saliba <mario.saliba98@gmail.com>
2023-12-28 21:36:34 +0200

    remove the stroke from shapes

--- src/lib/defaultShapes.tsx
+++ src/lib/defaultShapes.tsx
@@ -1,21 +1,21 @@
 import { ShapeParams } from './editor'
 
 export const STROKE = '#000000'
-export const FILL = 'rgba(255, 255, 255, 0.0)'
+export const FILL = 'rgba(0, 0, 0, 1)'
 
 export const CIRCLE = ({ x, y }: ShapeParams) => ({
   radius: 20,
   left: x || 100,
   top: y || 100,
-  fill: FILL,
-  stroke: STROKE
+  strokeWidth: 0,
+  fill: FILL
 })
 
 export const RECTANGLE = ({ x, y }: ShapeParams) => ({
   left: x || 100,
   top: y || 100,
   fill: FILL,
-  stroke: STROKE,
+  strokeWidth: 0,
   width: 40,
   height: 40,
   angle: 0
@@ -25,7 +25,7 @@ export const TRIANGLE = ({ x, y }: ShapeParams) => ({
   left: x || 100,
   top: y || 100,
   fill: FILL,
-  stroke: STROKE,
+  strokeWidth: 0,
   width: 40,
   height: 40,
   angle: 0
@@ -35,7 +35,7 @@ export const ARROW = ({ x, y }: ShapeParams) => ({
   left: x || 100,
   top: y || 100,
   fill: 'black',
-  strokeWidth: 4,
+  strokeWidth: 0,
   width: 50,
   height: 50
 })
