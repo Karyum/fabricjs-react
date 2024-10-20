67647d273618877b116f42c315d6f7bc84c9b8d2
Mario Saliba <mario.saliba98@gmail.com>
2024-10-20 11:27:14 +0300

    fixes arrow dimensions bug

--- src/lib/defaultShapes.tsx
+++ src/lib/defaultShapes.tsx
@@ -51,8 +51,8 @@ export const ARROW = ({ x, y }: ShapeParams) => ({
   fill: 'black',
   strokeWidth: 4,
   strokeUniform: true,
-  width: 50,
-  height: 50
+  width: 100,
+  height: 100
 })
 
 export const CUBE_FACE = ({ x, y }: ShapeParams) => ({
